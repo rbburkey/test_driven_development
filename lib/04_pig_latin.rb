d04ad733a353da8cb782a80a7135d81459ecaa69
rbburkey <robinsonbburkey@gmail.com>
2016-08-27 16:25:54 -0700

    finished lesson 4

--- lib/04_pig_latin.rb
+++ lib/04_pig_latin.rb
@@ -1,13 +1,43 @@
 def translate (input)
-  words = input.split(" ")
+  words = input.split
   vowels = ["a","e","i","o","u"]
-  words.each do |x|
+  exceptions = "qu"
+  arr = []
+    words.each do |x|
+
     if  vowels.include? x[0]
-      return x + x[0] + "y"
-    elsif (!(vowels.include? x[0]) && vowels.include? x[1])
-      return x.delete(x[0]) + x[0] + "ay"
-    else #!(vowels.include? x[0] && x[1])
-      return x.delete(x[0]) + x[0,1] + "ay"
+
+      arr << x + "ay"
+    else
+      arr << consonant_helper(x)
     end
   end
+  return arr.join(" ")
+
+end
+
+
+
+def consonant_helper (word)
+  vowels = ["a","e","i","o","u"]
+  consonants = ""
+  remaining_vowels = ""
+  vowel_index = 0
+  word_char = word.split(//)
+  word_char.each_with_index do |letter,index|
+    if !(vowels.include? (letter)) && !(letter == "q")
+      consonants += letter
+    elsif (letter == "q" && word_char[index + 1] == "u")
+      consonants += letter
+      consonants += word_char[index + 1]
+      vowel_index += index + 2
+      break
+    else
+      vowel_index += index
+      break
+    end
+  end
+  remaining_vowels += word[(vowel_index..-1)]
+  full_word = remaining_vowels + consonants + 'ay'
+
 end
