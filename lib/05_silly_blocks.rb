fd70dda75404c28cdc635b3d92dbc738009e5d0f
rbburkey <robinsonbburkey@gmail.com>
2016-08-27 20:53:50 -0700

    finished silly blocks

--- lib/05_silly_blocks.rb
+++ lib/05_silly_blocks.rb
@@ -1,3 +1,13 @@
-reverser = Proc.new {puts "hello"}
+def reverser(&block)
+  words = yield.split(" ")
+  transformed_words = words.inject(""){ |sum, word| sum += word.reverse + " "}
+  transformed_words.strip
+end
 
-reverser.call
+def adder(num=1, &block)
+  yield + num
+end
+
+def repeater(num=1, &block)
+  num.times { yield }
+end
